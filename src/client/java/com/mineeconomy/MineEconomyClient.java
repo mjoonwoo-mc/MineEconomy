9d2dd768a380fdd9b58a03606e29451879ade187
JoonwooM <mjoonwoo@gmail.com>
2025-08-18 16:44:11 +0900

    change version to v1.21.8

---
+++ src/client/java/com/mineeconomy/MineEconomyClient.java
@@ -0,0 +1,10 @@
+package com.mineeconomy;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MineEconomyClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/java/com/mjoonwoo/mineeconomy/item/ModItems.java
@@ -0,0 +1,31 @@
+package com.mjoonwoo.mineeconomy.item;
+
+import com.mjoonwoo.mineeconomy.MineEconomy;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.item.Items;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.util.Identifier;
+
+import java.util.function.Function;
+
+public class ModItems {
+
+    public static final Item CUSTOM_ITEM = registerItem("custom_item", Item::new, new Item.Settings());
+
+    public static Item registerItem(String name, Function<Item.Settings, Item> factory, Item.Settings settings) {
+        final RegistryKey<Item> registerKey = RegistryKey.of(RegistryKeys.ITEM, Identifier.of(MineEconomy.MOD_ID, name));
+        return Items.register(registerKey,factory, settings);
+    }
+
+    private static void customIngredients(FabricItemGroupEntries entries) {
+        entries.add(CUSTOM_ITEM);
+    }
+
+    public static void registerItems() {
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(ModItems::customIngredients);
+    }
+}
